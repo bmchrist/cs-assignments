c31132e45da480e2efa02be6e8e75f81f442d834
Ben Christensen <bmchrist@mtu.edu>
2014-04-18 10:21:31 -0400

    Let's get started

---
+++ 1141/5/huffman.c
@@ -0,0 +1,13 @@
+int main(int argc, char *argv[]){
+  // Get filename from argv
+  // open file
+  // n = read first line - number, this is number of nodes
+  // n times:
+    // read value, read key
+    // insert key into BST
+  
+  // until 2
+    // read bit
+    // traverse tree
+    // if leaf, print, move pointer back to root
+}
